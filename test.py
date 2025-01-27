d17f769928a2083748bacaf36f7d8f0c4031a11e
Bhavesh9480 <bhavesh101suthar@gmail.com>
2025-01-27 22:16:46 +0530

    String opretions

---
+++ test.py
@@ -0,0 +1,11 @@
+#Slicing
+#Break the string
+'''string = "hello, World!"
+print(len(string))
+print(string[-3:-1])  # Output: Hello
+print(string.endswith("d"))
+print(string.capitalize())
+print(string.replace("l","a"))
+print(string)
+print(string.find("W"))
+print(string.count("l"))'''
