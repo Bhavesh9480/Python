4aab90e7135e02109a3c4c473538e1e8968ab0b4
Bhavesh9480 <bhavesh101suthar@gmail.com>
2025-02-02 15:47:13 +0530

    nptel assignment week 1

--- test.py
+++ test.py
@@ -9,3 +9,23 @@ print(string.replace("l","a"))
 print(string)
 print(string.find("W"))
 print(string.count("l"))'''
+
+'''def f(x):
+    d=0
+    y=1
+    while y <= x:
+        d=d+1
+        y=y*3
+    return(d)
+
+print(f(8538))'''
+
+def h(n):
+    s = 0
+    for i in range(1,n+1):
+        if n%i > 0:
+           s = s+1
+    return(s)
+print(h(61)-h(60))  # Output: 4
+print(h(60))  # Output: 4
+
