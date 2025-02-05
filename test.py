c0a9f600a35a89f3a56241e914e782d72858e031
Bhavesh9480 <bhavesh101suthar@gmail.com>
2025-02-05 23:27:55 +0530

    Python List

--- test.py
+++ test.py
@@ -20,12 +20,26 @@ print(string.count("l"))'''
 
 print(f(8538))'''
 
-def h(n):
+'''def h(n):
     s = 0
     for i in range(1,n+1):
         if n%i > 0:
            s = s+1
     return(s)
 print(h(61)-h(60))  # Output: 4
-print(h(60))  # Output: 4
+print(h(60))  # Output: 4'''
 
+#age=(input('Age :'))
+#print (age)  # Output: Age :
+marks=[11,22,143 ,15,19,17]
+print(marks)
+print(type(marks))
+print(marks[2])
+#marks[2]="Suthar"
+print(marks[2])
+#print(marks[2:5])
+#marks.append("Hello")
+marks.reverse()
+#print(marks.sort())
+marks.pop(2)
+print(marks)
